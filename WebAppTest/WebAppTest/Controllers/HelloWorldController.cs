b5d0454ec2733baedfc405125c5a3ef5b973e09a
Matthew-Harding <matthew.sourcecode@gmail.com>
2017-10-18 15:24:34 +0100

    initial commit

---
+++ WebAppTest/WebAppTest/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebAppTest.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        [Route("sayhello/{id}")]
+        public string SayHello(string name, int id = 1)
+        {
+            return $"Hello {name}, you have an ID of: {id}";
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AnotherPage()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
